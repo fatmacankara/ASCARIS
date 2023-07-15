ec2c5ce46db3a4f564668b0f2c7223083b7363f8
fatmacankara <fatmacankaraa@gmail.com>
2023-07-15 09:04:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+
+import pandas as pd 
+from os import path
+import sys
+sys.path.append('code/')
+#sys.path.append('ASCARIS/code/') 
+#import pdb_featureVector
+#import alphafold_featureVector
+#import argparse
 st.write('Hello')
 st.write('ljl')
 st.write('aaaaaaa')
+source = st.selectbox('Source',[1,2])
+impute = st.selectbox('Impute',[True, False])
+input_data = st.text_input('Input')
